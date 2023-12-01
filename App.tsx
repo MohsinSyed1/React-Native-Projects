ebb196c911b5ac3dbe661c666638b4b813d672b6
Syed Mohsin Pasha <syedmohsin.1701@gmail.com>
2023-12-01 10:45:12 +0400

    commit 2

--- App.tsx
+++ App.tsx
@@ -13,13 +13,13 @@ function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <>
-      <View style={styles.container1}>
+      <View style={styles.container}>
         <Text style={isDarkMode ? styles.whiteText : styles.blackText}>
           Hello World!!!
         </Text>
-      </View>
-      <View style={styles.container2}>
-        <Text>Dev Mohsin Here</Text>
+        <Text style={isDarkMode ? styles.blackText : styles.whiteText}>
+          Dev Mohsin Here
+        </Text>
       </View>
     </>
   );
@@ -28,7 +28,7 @@ function App(): JSX.Element {
 export default App;
 
 const styles = StyleSheet.create({
-  container1: {
+  container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
@@ -36,17 +36,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     backgroundColor: '#f4f4f4',
   },
-  container2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: 'red',
-    borderWidth: 1,
-  },
   whiteText: {
     color: 'grey',
   },
   blackText: {
-    color: '#000000',
+    color: '#000',
   },
 });
